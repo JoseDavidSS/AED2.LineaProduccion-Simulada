a859d9fa2afe17c0c6ba2049681ca569afcfaac3
FabianRamirez03 <ramirezfabian003@gmail.com>
2019-04-19 11:37:38 -0600

    codigo simplificado

--- Logica/Lista.cpp
+++ Logica/Lista.cpp
@@ -107,8 +107,8 @@ void Lista::actualizar(){
     int i;
     int a =0;
     Nodo* tmp = this->head;
-    while(tmp!= NULL){
-        if(tmp->getEstado() == true)
+    while(tmp!= nullptr){
+        if(tmp->getEstado())
             tmp->setTiempo(tmp->getTiempo()-5);
             tmp = tmp->next;
             a++;
@@ -135,8 +135,8 @@ void Lista::actualizar(){
 }
 void Lista::verificarEstado(){
     Nodo* tmp = this->head;
-    while(tmp!= NULL){
-        if(tmp->getEstado() == true){
+    while(tmp!= nullptr){
+        if(tmp->getEstado()){
             tmp->setEstadoF();
             tmp->next->next->setEstadoT();
             break;
